59deca2a95a41787c5ccaabda9a385efae7c1788
JonGrabbe <jgrabbe69@gmail.com>
2023-09-14 04:33:50 -0700

    tablet menu nav items resonsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import logo from "../../assets/shared/desktop/logo-dark.png"
 import hamburgerMenuIcon from "../../assets/shared/mobile/icon-hamburger.svg";
+import closeMenuIcon from "../../assets/shared/mobile/icon-close.svg";
 
 function MenuItem(props) {
     return (
@@ -13,6 +14,7 @@ function MenuItem(props) {
 export default function Header(props) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     let menuClassName = isMenuOpen ? ' menu-open' : ' menu-closed';
+    // let menuIconChangeClick = isMenuOpen ? <img src={closeMenuIcon} alt="" /> : <img src={hamburgerMenuIcon} alt="" />
 
     function menuClick() {
         setIsMenuOpen(!isMenuOpen)
@@ -22,9 +24,16 @@ export default function Header(props) {
         <>
             <header className="header spacer">
                 <img src={logo} className="logo" alt="" />
-                <button onClick={menuClick}>
-                    <img src={hamburgerMenuIcon} alt="" />
+                <button onClick={menuClick} className='menu-button'>
+                    <img src={isMenuOpen ? closeMenuIcon : hamburgerMenuIcon} alt="" />
                 </button>
+                <nav className="header-nav-menu-container nav-ul-menu">
+                    <ul>
+                        <MenuItem text="Our company" href="/about" />
+                        <MenuItem text="Our Locations" />
+                        <MenuItem text="Our Contact" />
+                    </ul>
+                </nav>
             </header>
 
             <div className={"menu-dropdown-container"+menuClassName} id='menu-dropdown-container'>
