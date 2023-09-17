4050c276667fd7621c8f418155b99dde12325907
JonGrabbe <jgrabbe69@gmail.com>
2023-09-17 02:20:39 -0700

    added blur container

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,19 +1,22 @@
 import rightArrow from '../../assets/shared/desktop/icon-right-arrow.svg';
+import BlurContainer from './blur-container/BlurContainer';
 
 export default function Card(props) {
     return (
         <div className="card-container" style={{backgroundImage: `url(${process.env.PUBLIC_URL+'/'+props.imgPath})`}}>
-            <div className="content-inner-wrapper">
-                <h3 className="h3 heading">
-                    {props.text}
-                </h3>
-                <div className="link-container">
-                    <span className="text">
-                        view projects
-                    </span>
-                    <img src={rightArrow} alt="" className="arrow-img" />
+            <BlurContainer>
+                <div className="content-inner-wrapper">
+                    <h3 className="h3 heading">
+                        {props.text}
+                    </h3>
+                    <div className="link-container">
+                        <span className="text">
+                            view projects
+                        </span>
+                        <img src={rightArrow} alt="" className="arrow-img" />
+                    </div>
                 </div>
-            </div>
+            </BlurContainer>
         </div>
     )
 }
---
+++ src/components/card/blur-container/BlurContainer.js
@@ -0,0 +1,7 @@
+export default function BlurContainer(props) {
+    return (
+        <div className="blur-container">
+            {props.children}
+        </div>
+    )
+}
