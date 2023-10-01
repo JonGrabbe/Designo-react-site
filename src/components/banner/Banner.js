833afa836d12ad9c0890bc6d6d7f1d0420071cb4
JonGrabbe <jgrabbe69@gmail.com>
2023-10-01 14:59:05 -0700

    added flipped background images

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,11 +2,14 @@ import ButtonOnDark from '../buttons/ButtonOnDark';
 import smallCircle from '../../assets/shared/desktop/bg-pattern-small-circle.svg';
 
 export default function Banner(props) {
-    let smallCirclePath = '../../assets/shared/desktop/bg-pattern-small-circle.svg';
+    let smallCirclePath = '../../assets/shared/desktop/bg-pattern-small-circle.png';
     let circleWidth = 292;
+    let imgUrl = `url(${process.env.PUBLIC_URL}/assets/shared/desktop/bg-pattern-small-circle.png)`;
+    let backgroundImage = `${imgUrl}, ${imgUrl}, ${imgUrl}`
+    // backgroundImage: `url(${process.env.PUBLIC_URL}/assets/shared/desktop/bg-pattern-small-circle.svg), url(${process.env.PUBLIC_URL}/assets/shared/desktop/bg-pattern-small-circle.svg), url(${process.env.PUBLIC_URL}/assets/shared/desktop/bg-pattern-small-circle.svg)`,
     let styles = {
-        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/shared/desktop/bg-pattern-small-circle.svg), url(${process.env.PUBLIC_URL}/assets/shared/desktop/bg-pattern-small-circle.svg), url(${process.env.PUBLIC_URL}/assets/shared/desktop/bg-pattern-small-circle.svg)`,
-        backgroundPosition: `100px -80px, 100px 208px`,
+        backgroundImage: backgroundImage,
+        backgroundPosition: `100px -80px, 100px 208px, -100% 208px`,
         backgroundSize: "292px auto, 292px auto",
         backgroundRepeat: "no-repeat",
     }
