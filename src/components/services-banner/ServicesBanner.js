327cb1231986f3447554ab9e2b06772da5170a4f
JonGrabbe <jgrabbe69@gmail.com>
2023-10-02 21:37:14 -0700

    services banner finished

--- src/components/services-banner/ServicesBanner.js
+++ src/components/services-banner/ServicesBanner.js
@@ -1,10 +1,12 @@
 export default function ServicesBanner(props) {
     return (
-        <div className="services-banner banner">
-            <h1 className="heading h1">Graphic Design</h1>
-            <p className="text p">
-                We build websites that serve as powerful marketing tools and bring memorable brand experiences.
-            </p>
+        <div className="services-banner banner spacer-margin">
+            <div className="content">
+                <h1 className="heading h1">Graphic Design</h1>
+                <p className="text p">
+                    We deliver eye-catching branding materials that are tailored to meet your business objectives.
+                </p>
+            </div>
         </div>
     )
 }
