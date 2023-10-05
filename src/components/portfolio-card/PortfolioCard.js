c2fca1d40fe5bdffaf1be83673b804d9a68dc8c7
JonGrabbe <jgrabbe69@gmail.com>
2023-10-05 11:41:05 -0700

    fixed height problem

--- src/components/portfolio-card/PortfolioCard.js
+++ src/components/portfolio-card/PortfolioCard.js
@@ -1,29 +1,9 @@
 function PortfolioCard(props) {
-    // let imgPath = '../../assets/graphic-design/desktop/image-boxed-water.jpg'
-    // let imgPathBase = '../../assets/'
-    /* const imagesData = {
-        timBrown: {
-            imgPath: `${imgPathBase}graphic-design/desktop/image-boxed-water.jpg`,
-            category: 'graphicDesign',
-            order: 1
-        },
-        boxedWater: {
-            imgPath: `${imgPathBase}graphic-design/desktop/image-boxed-water.jpg`,
-            category: 'graphicDesign',
-            order: 2
-        },
-        science: {
-            imgPath: `${imgPathBase}graphic-design/desktop/image-boxed-water.jpg`,
-            category: 'graphicDesign',
-            order: 3
-        }     
-    } */
     return (
         <div className="portfolio-card-container">
-            {/* <div className="img-container"> */}
-                {/* <img src={require('../../assets/graphic-design/desktop/image-boxed-water.jpg')} alt="" /> */}
-            {/* </div> */}
+            <div className="img-container">
                 <img className="img" src={props.imgPath} alt="" />
+            </div>
             <div className="info-container">
                 <div className="content-wrapper">
                     <h2 className="heading h2">
