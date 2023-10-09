a09d48cc40d6eb21a55dd4dab0e299427685d8b3
JonGrabbe <jgrabbe69@gmail.com>
2023-10-08 17:31:59 -0700

    finished up cards container

--- src/components/about-page-borderless-cards/AboutPageBorderlessCards.js
+++ src/components/about-page-borderless-cards/AboutPageBorderlessCards.js
@@ -2,13 +2,13 @@ import BorderlessCard from "../borderless-card/BorderlessCard";
 
 function AboutPageBorderlessCardsContainer(props) {
     return (
-        <div className="borderless-cards-container spacer-margin">
+        <div className="borderless-cards-container about-page-borderless-cards-container spacer-margin">
             {/* <BorderlessCard imgType="passionate" text={passionateText} headingText="PASSIONATE" />
             <BorderlessCard imgType="resourceful" text={friendlyText} headingText="RESOURCEFUL" />
             <BorderlessCard imgType="friendly" text={resourcefulText} headingText="FRIENDLY" /> */}
-            <BorderlessCard headingText="CANADA" imgType='canada' />
-            <BorderlessCard headingText="AUSTRALIA" imgType='australia' />
-            <BorderlessCard headingText="UK" imgType='uk' />
+            <BorderlessCard headingText="CANADA" imgType='canada' hasButton={true} />
+            <BorderlessCard headingText="AUSTRALIA" imgType='australia' hasButton={true} />
+            <BorderlessCard headingText="UK" imgType='uk' hasButton={true} />
         </div>
     );
 }
--- src/components/borderless-card/BorderlessCard.js
+++ src/components/borderless-card/BorderlessCard.js
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react';
 import BackgroundImg from './background-img/BackgroundImg';
 
 export default function BorderlessCard(props) {
+    let button = props.hasButton ? (<button className='button--on-light'>See More</button>) : null;
+    let text = props.text ? (<p className='text p'>{props.text}</p>) : null;
+
     return (
         <div className="borderless-card" >
             <BackgroundImg imgType={props.imgType} />
             <h2 className="h2 heading">{props.headingText}</h2>
-            <p className="text p">
+            {/* <p className="text p">
                 {props.text}
-            </p>
+            </p> */}
+            {text ? text : null}
+            {button ? button : null}
         </div>
     )
 }
