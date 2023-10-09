80336467e89dd13e72f6cbe1824bf93725aae50b
JonGrabbe <jgrabbe69@gmail.com>
2023-10-08 17:55:16 -0700

    finished up bottom-banner

---
+++ src/components/about-banner-real-deal/AboutBannerRealDeal.js
@@ -0,0 +1,22 @@
+import AboutBannerMain from "../about-banner-main/AboutBannerMain";
+
+function AboutBannerRealDeal(props) {
+    return (
+      <AboutBannerMain cn="about-banner-right-image main-body-banner" heading="The Real Deal">
+        <p className="text p">
+          As strategic partners in our clients’ businesses, we are ready to take
+          on any challenge as our own. Solving real problems require empathy and
+          collaboration, and we strive to bring a fresh perspective to every
+          opportunity. We make design and technology more accessible and give
+          you tools to measure success.
+        </p>
+        <p className="text p">
+          zWe are visual storytellers in appealing and captivating ways. By
+          combining business and marketing strategies, we inspire audiences to
+          take action and drive real results.
+        </p>
+      </AboutBannerMain>
+    );
+}
+
+export default AboutBannerRealDeal;
--- src/routes/about/AboutPage.js
+++ src/routes/about/AboutPage.js
@@ -1,3 +1,4 @@
+import AboutBannerRealDeal from "../../components/about-banner-real-deal/AboutBannerRealDeal";
 import AboutBannerWorldClass from "../../components/about-banner-world-class/AboutBannerWorldClass";
 import AboutBanner from "../../components/about-banner/AboutBanner";
 import AboutPageBorderlessCardsContainer from "../../components/about-page-borderless-cards/AboutPageBorderlessCards";
@@ -8,6 +9,7 @@ function AboutPage(props) {
             <AboutBanner />
             <AboutBannerWorldClass />
             <AboutPageBorderlessCardsContainer />
+            <AboutBannerRealDeal />
         </>
     );
 }
