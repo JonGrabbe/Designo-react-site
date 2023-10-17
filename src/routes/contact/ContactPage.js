83d4163ae5c682f5ae68ded7c43bfcd48bde9d95
JonGrabbe <jgrabbe69@gmail.com>
2023-10-16 22:01:03 -0700

    added borderless cards

--- src/routes/contact/ContactPage.js
+++ src/routes/contact/ContactPage.js
@@ -1,10 +1,12 @@
 import ContactForm from "../../components/contact-form/ContactForm";
 import Wrapper from "../../components/wrapper/wrapper";
+import BorderlessCardsContainer from "../../components/borderless-cards-container/BorderlessCardsContainer";
 
 function ContactPage(props) {
     return (
         <>
             <ContactForm />
+            <BorderlessCardsContainer />
         </>
     );
 }
